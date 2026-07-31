3ba0bd0aba55cef21cce6e0e1d9fd7050ed5bc71
admanehocine <admanehocine05@gmail.com>
2026-07-31 19:52:28 +0200

    new commit

--- features/login.feature
+++ features/login.feature
@@ -1,4 +1,8 @@
 Feature: Connexion
 
   Scenario: Connexion réussie
-    Given test
+    Given visiter le site "https://www.saucedemo.com/"
+    And remplir le username "standard_user"
+    And remplir le password "secret_sauce"
+    When click sur button login
+    Then je suis dans la page accueil  
--- features/step_definitions/login.steps.ts
+++ features/step_definitions/login.steps.ts
@@ -1,6 +1,21 @@
-import { Given } from "@cucumber/cucumber";
+import { Given,Then,When } from "@cucumber/cucumber";
+import { CustomWorld } from "../support/world";
 
-Given("test", async function(){
-    await this.page.goto("https://www.saucedemo.com/")
+ 
 
+Given('visiter le site {string}',async function(this: CustomWorld,s: string)  {
+     await this.page.goto(s)
+
+})
+Given('remplir le password {string}', async function(this: CustomWorld,s: string)  {
+    await this.page.locator("#user-name").fill("s")
+})
+
+When('click sur button login', async function(this: CustomWorld,) {
+})
+
+Given('remplir le username {string}',async  function(this: CustomWorld,s: string) {
+})
+
+Then('je suis dans la page accueil', async function(this: CustomWorld,) {
 })
