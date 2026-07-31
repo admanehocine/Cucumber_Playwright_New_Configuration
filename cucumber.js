bfad0db19b6f9ecef33a1b6030e97dd6377e8c23
admanehocine <admanehocine05@gmail.com>
2026-07-31 20:52:19 +0200

    new commit

--- cucumber.js
+++ cucumber.js
@@ -3,8 +3,15 @@ module.exports = {
     requireModule: ['ts-node/register'],
     require: ['features/step_definitions/**/*.ts', 'features/support/**/*.ts'],
     paths: ['features/**/*.feature'],
-    format: ['progress-bar', 'html:reports/cucumber-report.html'],
+    format: [
+      'progress-bar',
+      'html:reports/cucumber-report.html',
+      'allure-cucumberjs/reporter'
+    ],
+    formatOptions: {
+      resultsDir: 'allure-results'
+    },
     publishQuiet: true,
-    timeout: 60000,
+    timeout: 60000
   }
-}; 
+};
